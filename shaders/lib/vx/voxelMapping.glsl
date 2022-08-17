98b8a6b5f2a6edbd1ca1a7715e4ba835c3d5cd86
gri573 <david.dg.grimm@gmail.com>
2022-08-17 12:57:39 +0200

    voxel mapping functions

---
+++ shaders/lib/vx/voxelMapping.glsl
@@ -0,0 +1,32 @@
+//// needs uniform vec3 cameraPosition, previousCameraPosition
+
+// voxel volume diameter
+const int vxRange = 2 * (shadowMapResolution / (2 * VXHEIGHT));
+
+// convert 3D position in voxel space to 2D position on the voxel map
+vec2 getVxCoords(vec3 voxelPos) {
+    voxelPos.y += VXHEIGHT * VXHEIGHT / 2;
+    ivec2 coords = voxelPos.xz + vxRange / 2;
+    coords.x += int(voxelPos.y) % VXHEIGHT * vxRange;
+    coords.y += int(voxelPos.y) / VXHEIGHT * vxRange;
+    return (coords + vec2(0.5)); / vec2(shadowMapResolution);
+}
+
+// inverse function to getVxCoords
+vec3 getVxPos(vec2 vxCoords) {
+    vxCoords *= shadowMapResolution;
+    vec3 pos;
+    pos.xz = mod(vxCoords, vec2(vxRange)) - vxRange / 2;
+    pos.y = floor(vxCoords.y / vxRange) * VXHEIGHT + floor(vxCoords.x / vxRange) - VXHEIGHT * VXHEIGHT / 2 + 0.5;
+    return pos;
+}
+
+// get voxel space position from world position
+vec3 getVxPos(vec3 worldPos) {
+    return worldPos + fract(cameraPosition);
+}
+
+// get previous voxel space position from world position
+vec3 getPreviousVxPos(vec3 worldPos) {
+    return worldPos + (cameraPosition - floor(PreviousCameraPosition));
+}
